4660b5623a6d9684130ada5451f7fbbb2c72ff54
bago2k4 <iacopo@rc0.eu>
2018-09-26 23:53:03 +0200

    Carrot theme update.

--- dist/betajs-media-components-noscoped.js
+++ dist/betajs-media-components-noscoped.js
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.131 - 2018-08-09
+betajs-media-components - v0.0.132 - 2018-09-26
 Copyright (c) Ziggeo,Oliver Friedmann
 Apache-2.0 Software License.
 */
@@ -15,7 +15,7 @@ Scoped.binding('dynamics', 'global:BetaJS.Dynamics');
 Scoped.define("module:", function () {
 	return {
     "guid": "7a20804e-be62-4982-91c6-98eb096d2e70",
-    "version": "0.0.131"
+    "version": "0.0.132"
 };
 });
 Scoped.assumeVersion('base:version', '~1.0.96');
